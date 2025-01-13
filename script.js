e8ea5e87716323c61e26f55963fcd158580ef50f
N3rd <landonmarting@icloud.com>
2025-01-12 20:31:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -28,8 +28,13 @@ const tabs = document.createElement('div');
 tabs.style.display = 'flex';
 
 // Create tab buttons
-const tabNames = ['Tab 1', 'Tab 2', 'Tab 3'];
-const tabContents = ['Content for Tab 1', 'Content for Tab 2', 'Content for Tab 3'];
+const tabNames = ['object worth', 'Tab 2', 'Tab 3'];
+const tabContents = ['/ Object value upgrade button
+const objectValueUpgrade = document.createElement('button');
+objectValueUpgrade.innerText = "Upgrade object worth";
+objectValueUpgrade.style.position = 'absolute'; // objectValueUpgrade.style.left = '475px'; 
+objectValueUpgrade.style.top = '100px';
+document.body.appendChild(objectValueUpgrade);, 'Content for Tab 2', 'Content for Tab 3'];
 const tabButtons = [];
 
 tabNames.forEach((name, index) => {
